7b86bb4f441459470903fa1b9f7d9ed59cc3ac51
Andi Büchler <andi.buechler@gmail.com>
2015-01-20 14:10:43 +0100

    Adding nvd3 directive in the demo.

--- demo/script.js
+++ demo/script.js
@@ -6,7 +6,7 @@
  */
 
 
-var demoApp = angular.module('demoApp', ['angularPanel']);
+var demoApp = angular.module('demoApp', ['angularPanel', 'nvd3']);
 
 
 demoApp.directive('someOtherDirective', ['$log',
@@ -31,7 +31,12 @@ demoApp.controller('PanelCtrl', ['$scope', '$log',
 
     $log.debug('PanelCtrl setup...');
 
-    $scope.panel = {
+    $scope.panel0 = {
+      title: 'Dynamic Bootstrap Panel with nvd3 Directive',
+      collapsed: false
+    };
+
+    $scope.panel1 = {
       title: 'Dynamic Bootstrap Panel',
       collapsed: false,
       content: 'The panel\'s content is another directive.'
@@ -57,6 +62,78 @@ demoApp.controller('PanelCtrl', ['$scope', '$log',
     $scope.panelRemove = function () {
       alert('Remove');
     };
+
+
+    // The Nvd3 Demo was taken from http://plnkr.co/edit/EVW5qF?p=preview
+
+    $scope.options = {
+      chart: {
+        type: 'multiBarChart',
+        height: 600,
+        margin: {
+          top: 20,
+          right: 25,
+          bottom: 60,
+          left: 25
+        },
+        clipEdge: true,
+        staggerLabels: true,
+        transitionDuration: 1000,
+        tooltips: true,
+        tooltipContent: function (key, x, y, e, graph) {
+          return '<p>' + key + ': ' + y + '</p>';
+        },
+        stacked: true,
+        showControls: false,
+        xAxis: {
+          axisLabel: 'Time',
+          showMaxMin: true,
+          tickFormat: function (d) {
+            return d;
+          }
+        },
+        yAxis: {
+          axisLabel: 'Number of emails',
+          axisLabelDistance: 100,
+          tickFormat: function (d) {
+            return d3.format(',.f')(d);
+          }
+        }
+      }
+    };
+
+    $scope.data = generateData();
+
+    function generateData() {
+      var values = [];
+      var values0 = [];
+      var values1 = [];
+      //change number of bars here by editing '90'//
+      for (var h = 0; h < 90; h++) {
+        //replace the y values with your own values//
+        values.push({x: h, y: Math.random() + 1});
+        values0.push({x: h, y: Math.sqrt(h) / 2});
+        values1.push({x: h, y: Math.abs(h - 18)})
+      }
+
+      return [
+        {
+          key: 'Sent',
+          color: '#bcbd22',
+          values: values
+        },
+        {
+          key: 'Received',
+          color: '#1f77b4',
+          values: values0
+        },
+        {
+          key: 'Spam',
+          color: 'black',
+          values: values1
+        }
+      ];
+    }
 
 
   }
