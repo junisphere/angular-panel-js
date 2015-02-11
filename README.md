74549c34c62e1db5ef343134fc67f340d40fcb97
Werner Nickl <werni@gmx.de>
2015-02-11 10:54:36 +0100

    v0.0.1 RELEASE

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# angular-panel
-AngularJS version of Bootstrap Panel
+# angular-panel-js
+Junisphere AngularJS version of Bootstrap Panel
+a fork of https://github.com/abuechler/angular-panel
+
+v0.0.1 initial release:
+- fork
+- added place holders for i18n with https://github.com/angular-translate/angular-translate for mouse tips for header icons
--- dist/angular-panel.js
+++ dist/angular-panel.js
@@ -63,33 +63,33 @@ angular.module('angularPanel').run(['$templateCache', function ($templateCache) 
   'use strict';
 
   $templateCache.put('templates/ab-panel/panel.tpl.html',
-      "<div class=\"panel panel-primary\">\n" +
-      "    <div class=\"panel-heading\">\n" +
-      "        <div class=\"pull-right\">\n" +
-      "\n" +
-      "            <a href=\"\" ng-click=\"onRefreshClicked()\">\n" +
-      "                <span class=\"ab-glyphicon-color glyphicon glyphicon-refresh\"></span>\n" +
-      "            </a>\n" +
-      "\n" +
-      "            <a href=\"\" ng-click=\"onEditClicked()\">\n" +
-      "                <span class=\"ab-glyphicon-color glyphicon glyphicon-cog\"></span>\n" +
-      "            </a>\n" +
-      "            <a href=\"\" ng-click=\"onCollapseClicked()\">\n" +
-      "        <span\n" +
-      "                class=\"ab-glyphicon-color glyphicon {{ panelModel.collapsed ? 'glyphicon-plus' : 'glyphicon-minus' }}\"></span>\n" +
-      "            </a>\n" +
-      "\n" +
-      "            <a href=\"\" ng-click=\"onRemoveClicked()\">\n" +
-      "                <span style=\"margin-left: 20px;\" class=\"ab-glyphicon-color glyphicon glyphicon-remove\"></span>\n" +
-      "            </a>\n" +
-      "\n" +
-      "        </div>\n" +
-      "        <!-- Order does matter here - title must come last -->\n" +
-      "        <h3 class=\"panel-title\">{{panelModel.title}}</h3>\n" +
-      "    </div>\n" +
-      "\n" +
-      "\n" +
-      "</div>\n"
+    "<div class=\"panel panel-primary\">\n" +
+    "    <div class=\"panel-heading\">\n" +
+    "        <div class=\"pull-right\">\n" +
+    "\n" +
+    "            <a href=\"\" ng-click=\"onRefreshClicked()\" title=\"{{'LABEL_PANEL_HEADER_ICON_REFRESH' | translate}}\">\n" +
+    "                <span class=\"ab-glyphicon-color glyphicon glyphicon-refresh\"></span>\n" +
+    "            </a>\n" +
+    "\n" +
+    "            <a href=\"\" ng-click=\"onEditClicked()\" title=\"{{'LABEL_PANEL_HEADER_ICON_SETTINGS' | translate}}\">\n" +
+    "                <span class=\"ab-glyphicon-color glyphicon glyphicon-cog\"></span>\n" +
+    "            </a>\n" +
+    "            <a href=\"\" ng-click=\"onCollapseClicked()\" title=\"{{'LABEL_PANEL_HEADER_ICON_COLLAPSE' | translate}}\">\n" +
+    "        <span\n" +
+    "                class=\"ab-glyphicon-color glyphicon {{ panelModel.collapsed ? 'glyphicon-plus' : 'glyphicon-minus' }}\"></span>\n" +
+    "            </a>\n" +
+    "\n" +
+    "            <a href=\"\" ng-click=\"onRemoveClicked()\" title=\"{{'LABEL_PANEL_HEADER_ICON_REMOVE' | translate}}\">\n" +
+    "                <span style=\"margin-left: 20px;\" class=\"ab-glyphicon-color glyphicon glyphicon-remove\"></span>\n" +
+    "            </a>\n" +
+    "\n" +
+    "        </div>\n" +
+    "        <!-- Order does matter here - title must come last -->\n" +
+    "        <h3 class=\"panel-title\">{{panelModel.title}}</h3>\n" +
+    "    </div>\n" +
+    "\n" +
+    "\n" +
+    "</div>\n"
   );
 
 
